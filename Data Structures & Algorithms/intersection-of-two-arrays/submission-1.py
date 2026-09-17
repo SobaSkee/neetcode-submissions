ee7e590b9b126dfb848ec19dab7cecd1f0d22d19
Stanley Ke <117794857+SobaSkee@users.noreply.github.com>
2026-09-17 16:17:58 -0400

    Add: intersection-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        search = []
+        shorter = []
+        # [1,1,2,2] and [2,2]
+        if len(nums1) > len(nums2):
+            search = nums1
+            shorter = nums2
+        else:
+            search = nums2
+            shorter = nums1
+        res = []
+        l = 0
+        r = len(search)-1
+        for num in shorter:
+            if num in res:
+                continue
+            while l <= r:
+                mid = (r+l)//2
+                if search[mid] == num:
+                    res.append(num)
+                    break
+                elif search[mid] > num:
+                    r = mid-1
+                else:
+                    l = mid+1         
+            l = 0
+            r = len(search)-1
+        return res
+
+            
